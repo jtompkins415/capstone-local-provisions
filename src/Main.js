f9719fdef8b1f3122205e5552d928e99da9cdc29
jtompkins415 <john.tompkins415@gmail.com>
2023-02-14 14:26:19 -0800

    write component for main page

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+import NavBar from './NavBar';
+import {
+    Button
+} from 'reactstrap';
+import image from './SFcityscape.jpeg'
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div className="Main">
+            <div className='Main-title'>
+                <h1>LOCAL PROVISIONS</h1>
+            </div>
+            <div className="Main-title-img">
+                <img src={image} alt="city scape"/>
+            </div>
+            <div className="Main-title-question">
+                <h3>WHAT'S YOUR REGION?</h3>
+            </div>
+            <div className="Main-body">
+                <a className='Main-location-link' href='/sf-bayarea'>SF / BAY AREA</a>
+                <a className='Main-location-link' href='/los-angeles'>LOS ANGELES</a>
+                <a className='Main-location-link' href='/new-york'>NEW YORK</a>
+                <a className='Main-location-link' href='/chicago'>CHICAGO</a>
+            </div>
+        </div>
+    )
+}
+ 
+export default Main;

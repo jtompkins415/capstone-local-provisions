ab1b3c33648e9d38791ba368126d5ce178c33850
jtompkins415 <john.tompkins415@gmail.com>
2023-02-14 14:22:50 -0800

    start backend app

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.use(function(err, req, res, next) {
+    // the default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
+  
+    // set the status and alert the user
+    return res.status(status).json({
+      error: {message, status}
+    });
+  });
+
+module.exports = app;

892379d90207daf6507324668dd008e7a447a5f6
jtompkins415 <john.tompkins415@gmail.com>
2023-02-14 14:26:51 -0800

    write Navbar component

---
+++ src/NavBar.js
@@ -0,0 +1,26 @@
+import {
+    Navbar,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        
+            <Nav className="Navbar" justified>
+                <NavItem>
+                    <NavLink className='nav-link' href='/main'> HOME </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className="nav-link" href='/signup'> SIGN UP </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className="nav-link" href='/login'> LOGIN </NavLink>
+                </NavItem>
+            </Nav>
+    )
+};
+
+export default NavBar;

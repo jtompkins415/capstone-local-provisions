21b8c8d3616f135efeda328a35e8c5468cc5b892
jtompkins415 <john.tompkins415@gmail.com>
2023-02-14 15:06:58 -0800

    change stylesheetdependency

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -7,7 +7,7 @@ import {
     Button,
     Col
 } from 'reactstrap';
-import './SignUpForm.css'
+import './Form.css'
 
 const SignUpForm = ({signup}) => {
     
@@ -27,44 +27,44 @@ const SignUpForm = ({signup}) => {
 
     return (
         <div>
-            <div className='SignupForm-title'>
+            <div className='form-title'>
                 <h1> CREATE AN ACCOUNT </h1>
             </div>
-        <Form className='SignupForm'>
+        <Form className='form'>
             <FormGroup row>
-                <Label className="SignupForm-label" htmlFor='firstName' sm={3}>FIRST NAME: </Label>
+                <Label className="form-label" htmlFor='firstName' sm={3}>FIRST NAME: </Label>
                 <Col sm={8}>
-                <Input className='SignupForm-input' id='firstName' name='firstName' type='text' value={formData.firstName} onChange={handleChange} />
+                <Input className='form-input' id='firstName' name='firstName' type='text' value={formData.firstName} onChange={handleChange} />
                 </Col>
             </FormGroup>
             <FormGroup row>
-                <Label className="SignupForm-label" htmlFor='lastName' sm={3}>LAST NAME: </Label>
+                <Label className="form-label" htmlFor='lastName' sm={3}>LAST NAME: </Label>
                 <Col sm={8}>
-                <Input className='SignupForm-input'  id='lastName' name='lastName' type='text' value={formData.lastName} onChange={handleChange}/>
+                <Input className='form-input'  id='lastName' name='lastName' type='text' value={formData.lastName} onChange={handleChange}/>
                 </Col>
             </FormGroup>
             <FormGroup row>
-                <Label className="SignupForm-label" htmlFor='email' sm={3}>EMAIL: </Label>
+                <Label className="form-label" htmlFor='email' sm={3}>EMAIL: </Label>
                 <Col sm={8}>
-                <Input className='SignupForm-input'  id='email' name='email' type='email' value={formData.email} onChange={handleChange}/>
+                <Input className='form-input'  id='email' name='email' type='email' value={formData.email} onChange={handleChange}/>
                 </Col>
             </FormGroup>
             <FormGroup row>
-                <Label className="SignupForm-label" htmlFor='username' sm={3}>USERNAME: </Label>
+                <Label className="form-label" htmlFor='username' sm={3}>USERNAME: </Label>
                 <Col sm={8}>
-                <Input className='SignupForm-input'  id='username' name='username' type='text' value={formData.username} onChange={handleChange} />
+                <Input className='form-input'  id='username' name='username' type='text' value={formData.username} onChange={handleChange} />
                 </Col>
             </FormGroup>
             <FormGroup row>
-                <Label className="SignupForm-label" htmlFor='password' sm={3}>PASSWORD: </Label>
+                <Label className="form-label" htmlFor='password' sm={3}>PASSWORD: </Label>
                 <Col sm={8}>
-                <Input className='SignupForm-input'  id='password' name='password' type='password' value={formData.password} onChange={handleChange} />
+                <Input className='form-input'  id='password' name='password' type='password' value={formData.password} onChange={handleChange} />
                 </Col>
             </FormGroup>
-            <Button outline className="SignupForm-button" color="success">
-                Submit
+            <Button outline className="form-button" color="success">
+                Sign Up
             </Button>
-            <Button outline className="SignupForm-button" color='danger' href="/main">
+            <Button outline className="form-button" color='danger' href="/main">
                 Cancel
             </Button>
         </Form>

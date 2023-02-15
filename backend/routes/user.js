0e44d185f072fa36bea2d419e24b46f17dea540c
jtompkins415 <john.tompkins415@gmail.com>
2023-02-15 09:13:18 -0800

    write GET, POST, PATCH, DELETE routes for user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const {createToken} = require('../helpers/token');
 const router = new express.Router();
 const {
     NotFoundError,
@@ -7,12 +8,87 @@ const {
     UnauthorizedError
 } = require('../expressError');
 
-router.get('/signup', async (req, res, next) => {
+
+/**GET: Get details on a user 
+ * 
+ * Return user information
+*/
+
+router.get('/:username', async (req, res, next) => {
     try{
-        const result = await User.register(req.body);
-        return res.status(201).json({result});
+        const {username} = req.params;
+        const user = await User.get(username);
+
+        return res.status(200).json({user})
+    } catch(err){
+        return next(err);
+    }
+})
+
+
+/** POST: Register user 
+ * 
+ * Return token for future authentication
+ * 
+ */
+
+router.post('/register', async (req, res, next) => {
+    try{
+        const newUser = await User.register({...req.body, isAdmin:false});
+        const newToken = createToken(newUser);
+        return res.status(201).json({newToken});
     } catch(err){
         return next(err)
     }
 });
 
+/** GET: Authenticate User
+ * 
+ * 
+ */
+
+router.get('/token', async (req, res, next) => {
+    try{
+        const {username, password} = req.body;
+        const user = await User.authenticate(username, password);
+        const token = createToken(user);
+        return res.status(200).json({token});
+    } catch(err) {
+        return next(err);
+    }
+})
+
+/** PATCH: Update User information
+ * 
+ * 
+ */
+
+router.patch('/:username/update', async (req, res, next) => {
+    try{
+        const {username} = req.params;
+        const user = await User.update(username, {...req.body});
+
+        return res.status(204).json({user});
+    } catch(err){
+        return next(err);
+    }
+});
+
+/** DELETE: Delete user
+ * 
+ * Throw NotFound Error if no user
+ */
+
+router.delete('/:username', async (req, res, next) => {
+    try{
+        const {username} = req.params;
+
+        await User.remove(username);
+
+        return res.status(204).json({deleted: username});
+    } catch(err) {
+        return next(err);
+    }
+}) 
+
+module.export = router

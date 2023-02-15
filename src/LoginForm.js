37b58766c153dcf9b3afb7cd2b7fa1620d367859
jtompkins415 <john.tompkins415@gmail.com>
2023-02-15 14:19:42 -0800

    write function to handle form submission

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {
     Form,
     FormGroup,
@@ -10,34 +11,45 @@ import {
 import './Form.css'
 
 const LoginForm = ({login}) => {
-    
+    const navigate = useNavigate()
     const [formData, setFormData] = useState({
         username: '',
         password: ''
     })
-    
+    const [formErrors, setFormErrors] = useState([])
+
+
     const handleChange = (evt) => {
         const {name, value} = evt.target
         setFormData(data => ({...data,[name]:value}));
     }
 
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        let res = await login(formData);
+        if(res.success){
+            return navigate('/main')
+        } else {
+            setFormErrors(res.errors)
+        }
+    }
 
     return (
         <div>
             <div className='form-title'>
                 <h1> LOGIN </h1>
             </div>
-        <Form className='form'>
+        <Form className='form' onSubmit={handleSubmit}>
             <FormGroup row>
                 <Label className="form-label" htmlFor='username' sm={3}>USERNAME: </Label>
                 <Col sm={8}>
-                <Input className='form-input'  id='username' name='username' type='text' value={formData.username} onChange={handleChange} />
+                <Input className='form-input'  id='username' name='username' type='text' autoComplete='current-username' value={formData.username} onChange={handleChange} />
                 </Col>
             </FormGroup>
             <FormGroup row>
                 <Label className="form-label" htmlFor='password' sm={3}>PASSWORD: </Label>
                 <Col sm={8}>
-                <Input className='form-input'  id='password' name='password' type='password' value={formData.password} onChange={handleChange} />
+                <Input className='form-input'  id='password' name='password' type='password' autoComplete='current-password' value={formData.password} onChange={handleChange} />
                 </Col>
             </FormGroup>
             <Button outline className="form-button" color="success">

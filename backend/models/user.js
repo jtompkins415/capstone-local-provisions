55bbadf61aa07a69acbbf4bbb4d9d31b26840ab2
jtompkins415 <john.tompkins415@gmail.com>
2023-02-15 14:07:40 -0800

    import bcrypt and work factor variable

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const db = require('../db');
+const bcrypt = require('bcrypt');
+const {BCRYPT_WORK_FACTOR} = require('../config');
 const {sqlForPartialUpdate} = require('../helpers/sql');
 const {
     NotFoundError,

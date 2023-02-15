360dfd4e3a751a7ecf7198d17b634d9ecb3285e7
jtompkins415 <john.tompkins415@gmail.com>
2023-02-15 09:12:37 -0800

    add in new dependencies, user routes, 404 error handling

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
+
+const {NotFoundError} = require('./expressError');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+//User Routes
+app.use('/user', userRoutes);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
 
 app.use(function(err, req, res, next) {
     // the default status is 500 Internal Server Error

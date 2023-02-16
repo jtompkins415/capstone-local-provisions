3df00b2e70e99be7c563823a6184760ed01e904d
jtompkins415 <john.tompkins415@gmail.com>
2023-02-15 19:37:25 -0800

    create object to hold image urls

---
+++ src/regionMap.js
@@ -0,0 +1,11 @@
+import imgSF from './SFcityscape.jpeg';
+import imgLA from './LAcityscape.jpeg';
+
+/** Object to hold region names and their cooresponding image url */
+
+const regionMap = {
+    'san francisco': imgSF,
+    'los angeles': imgLA
+};
+
+export default regionMap;

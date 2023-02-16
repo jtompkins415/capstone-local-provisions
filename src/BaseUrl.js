192a86d002575b0213766988c1fdd7b28c64bd91
jtompkins415 <john.tompkins415@gmail.com>
2023-02-16 09:53:19 -0800

    change base url to tallk to localhost

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,4 +1,6 @@
-const BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+// const BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+
+const BASE_URL = 'http://localhost:3001/maps/api/place/nearbysearch/'
 
 export default BASE_URL;
 

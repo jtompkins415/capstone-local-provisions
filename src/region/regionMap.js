c85ad3f595d25736a08e712a8cdb30a6cc1d85e4
jtompkins415 <john.tompkins415@gmail.com>
2023-02-17 11:02:25 -0800

    add NY and Chicago images to regionMap

--- src/region/regionMap.js
+++ src/region/regionMap.js
@@ -1,11 +1,17 @@
 import imgSF from '../SFcityscape.jpeg';
 import imgLA from '../LAcityscape.jpeg';
+import imgNY from '../NYcityscape.jpeg';
+import imgChi from '../Chicagocityscape.jpeg';
+
 
 /** Object to hold region names and their cooresponding image url */
 
 const regionMap = {
     'san francisco': imgSF,
-    'los angeles': imgLA
+    'los angeles': imgLA,
+    'new york': imgNY,
+    'chicago': imgChi
+    
 };
 
 export default regionMap;

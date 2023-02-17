29ec56793f04c2f38debb03b33f828b97aa396a5
jtompkins415 <john.tompkins415@gmail.com>
2023-02-17 10:59:32 -0800

    add variable for photo api

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,6 +1,10 @@
 // const BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
 
-const BASE_URL = 'http://localhost:3001/maps/api/place/nearbysearch/'
+const BASE_URL_NEARBY = 'http://localhost:3001/maps/api/place/nearbysearch/'
+const BASE_URL_PHOTO = 'http://localhost:3001/maps/api/place/photo'
 
-export default BASE_URL;
+export {
+    BASE_URL_NEARBY,
+    BASE_URL_PHOTO
+};
 

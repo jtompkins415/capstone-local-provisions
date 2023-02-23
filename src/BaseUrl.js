a0d449779d10fea30727a1a80bbe471a6aefff57
jtompkins415 <john.tompkins415@gmail.com>
2023-02-22 17:29:04 -0800

    add base url for places details

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -2,9 +2,11 @@
 
 const BASE_URL_NEARBY = 'http://localhost:3001/maps/api/place/nearbysearch/'
 const BASE_URL_PHOTO = 'http://localhost:3001/maps/api/place/photo'
+const BASE_URL_DETAILS = 'http://localhost:3001/maps/api/place/details'
 
 export {
     BASE_URL_NEARBY,
-    BASE_URL_PHOTO
+    BASE_URL_PHOTO,
+    BASE_URL_DETAILS
 };
 

5118da61d9d36dadba15d7ccb49e160a12f2c1a8
jtompkins415 <john.tompkins415@gmail.com>
2023-02-23 21:14:50 -0800

    Add redirect for root route, move profile route to end

--- src/routing-nav/Routing.js
+++ src/routing-nav/Routing.js
@@ -1,26 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "../Home";
 import Main from "../Main";
 import SignUpForm from "../SignUpForm";
 import LoginForm from "../LoginForm";
 import Region from "../region/Region";
 import About from "../About";
+import Profile from "../Profile";
 
 
-const Routing =  ({login, signup}) =>{
+const Routing =  ({login, signup, username}) =>{
     return (
         <>
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Navigate to='/main' />} />
                 <Route path='/signup' element={<SignUpForm signup={signup}/>} />
                 <Route path='/main' element={<Main />} />
-                <Route path='/login' element={<LoginForm login={login}/>} />
+                <Route path='/login' element={<LoginForm login={login}/>} /> 
+               
                 <Route path='/sf-bayarea' element={<Region regName='san francisco'/>} />
                 <Route path='/los-angeles' element={<Region regName='los angeles'/>} />
                 <Route path='/new-york' element={<Region regName='new york'/>} />
                 <Route path='/chicago' element={<Region regName='chicago'/>} />
-                <Route path='/about' element={<About />} />
+                <Route path='/about' element={<About />} /> 
+                <Route exact path='/details/:username' element={<Profile />} /> 
             </Routes>
         </BrowserRouter>
         </>

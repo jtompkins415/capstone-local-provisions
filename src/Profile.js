8b61c6e60b24d9620b25d7b6c4fd939e2d1e5fd2
jtompkins415 <john.tompkins415@gmail.com>
2023-02-23 21:13:44 -0800

    Create Profile component

---
+++ src/Profile.js
@@ -0,0 +1,36 @@
+import {  useContext, useState } from "react";
+import UserContext from "./UserContext";
+import './Profile.css';
+
+
+
+const Profile = () => {
+
+    const [favorites, setFavorites] = useState([]);
+    const {currentUser} = useContext(UserContext);
+    
+    
+    const {username, firstName, lastName, email} = currentUser;   
+    
+    return (
+        <div className='Profile'>
+            <header className='Profile-header'>
+                <h1>Account Details</h1>
+            </header>
+            <div className='Profile-body'>
+                <h3>{firstName} {lastName}</h3>
+                <ul>
+                    <li>
+                        Username: {username}
+                    </li>
+                    <li>
+                        email: {email}
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
